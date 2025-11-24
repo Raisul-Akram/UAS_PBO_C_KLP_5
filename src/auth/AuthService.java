3780b357335ae08d27fd1ebc7fe7a0c832345e2f
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-11-24 16:49:08 +0700

    Implement login method in AuthService

--- src/auth/AuthService.java
+++ src/auth/AuthService.java
@@ -12,3 +12,16 @@ public class AuthService {
     public AuthService() {
         this.userRepo = new UserRepository();
     }
+
+    public Account login(String username, String password) throws LoginFailedException {
+        Account acc = userRepo.findByUsername(username);
+
+        if (acc == null)
+            throw new LoginFailedException("Username tidak ditemukan.");
+
+        if (!acc.getPassword().equals(password))
+            throw new LoginFailedException("Password salah.");
+
+        return acc; // return User atau Admin sesuai data
+    }
+}
