393b494f06e7b4f03f13df169bd8ba743fb86654
Raisul akram <akramraisul4@gmail.com>
2025-11-24 16:16:08 +0700

    Main

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,9 @@ import cli.MainMenu;
  * Main class untuk menjalankan aplikasi Sistem Pemesanan Tiket Bioskop.
  * @author Kelompok 5
  */
+public class Main {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.display();
+    }
+}
