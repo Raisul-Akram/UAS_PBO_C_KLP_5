833714c6e8c9a7c331e1e1265ff79dba5e7aac2d
MuhammadRazi07 <muhammadrazi072006@gmail.com>
2025-11-24 17:28:35 +0700

    Booking
    Implement Booking class to manage booking data.

---
+++ src/model/Booking.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.List;
+
+/**
+ * Class untuk menyimpan data pemesanan.
+ * @author Kelompok 5
+ */
+public class Booking {
+    private String bookingCode;
+    private String userId;
+    private String showtimeId;
+    private List<String> seats;
+    private double totalPrice;
+
+    public Booking(String bookingCode, String userId, String showtimeId, List<String> seats, double totalPrice) {
+        this.bookingCode = bookingCode;
+        this.userId = userId;
+        this.showtimeId = showtimeId;
+        this.seats = seats;
+        this.totalPrice = totalPrice;
+    }
