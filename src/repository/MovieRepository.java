cac1a24e66876541e2a4fae99fc3673a522471ce
cutwynona <wynonacut@gmail.com>
2025-11-24 17:05:15 +0700

    Update MovieRepository.java

--- src/repository/MovieRepository.java
+++ src/repository/MovieRepository.java
@@ -1,1 +1,21 @@
+package repository;
+
+import model.Movie;
+import util.FileManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Repository untuk Movie (CRUD lengkap + persistence).
+ * Versi Sempurna.
+ * @author 
+ */
+public class MovieRepository {
+    private List<Movie> movies = new ArrayList<>();
+    private final String FILE_PATH = "data/movies.txt";
+
+    public MovieRepository() {
+        loadFromFile();
+    }
 
