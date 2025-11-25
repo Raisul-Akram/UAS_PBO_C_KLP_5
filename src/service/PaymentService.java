94ccc264822d1dffbda6d7b97cfe9e9635190182
keiveenaldiandra <keiveenaldiandra23utama@gmail.com>
2025-11-25 12:08:03 +0700

    PaymentService.java

--- src/service/PaymentService.java
+++ src/service/PaymentService.java
@@ -6,7 +6,6 @@ import model.Payment;
  * Service untuk pembayaran.
  * @author Kelompok 5
  */
-
 public class PaymentService {
     public boolean process(Payment payment, double amount) {
         return payment.processPayment(amount);
