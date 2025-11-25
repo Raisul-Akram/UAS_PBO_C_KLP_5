2ad50bd864dd7e6e920fd57e37848bc5e9ccfcc3
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-11-25 11:33:08 +0700

    Remove unnecessary blank line in User.java

--- src/model/User.java
+++ src/model/User.java
@@ -4,7 +4,7 @@ public class User extends Account {
     public User(String id, String username, String password) {
         super(id, username, password);
     }
-    
+
     @Override
     public void showMenu() {
         System.out.println("User Menu");
